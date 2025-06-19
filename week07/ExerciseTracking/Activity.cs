6f1ec8ecff983908a493e01d71a46048c3d16195
mirianukeje <ukejemirian@gmail.com>
2025-06-19 09:05:34 +0100

    week7

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,24 @@
+public abstract class Activity
+{
+    private string _date;
+    private int _lengthInMinutes;
+
+    public Activity(string date, int lengthInMinutes)
+    {
+        _date = date;
+        _lengthInMinutes = lengthInMinutes;
+    }
+
+    public string GetDate() => _date;
+    public int GetMinutes() => _lengthInMinutes;
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_lengthInMinutes} min) - " +
+               $"Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+    }
+}
---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,25 @@
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int lengthInMinutes, int laps)
+        : base(date, lengthInMinutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return (_laps * 50.0) / 1000.0;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+}
